3737a351b1faf285b67712d1d66b77202e4e193d
IMrMachineI <fubu96meex@gmail.com>
2017-04-09 12:08:30 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Producto nuevoProducto;
+        Almacen nuevoAlmacen = new Almacen();
+        private void lblCantidad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt32(txtCodigo.Text);
+            string nombre = txtNombre.Text;
+            int cantidad = Convert.ToInt32(txtCantidad.Text);
+            int costo = Convert.ToInt32(txtCosto.Text);
+            nuevoProducto = new Producto(codigo, nombre, cantidad, costo);
+            nuevoAlmacen.agregarProducto(nuevoProducto);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Producto productoBuscado = nuevoAlmacen.buscarProducto(Convert.ToInt16(txtCodigo.Text));
+            if (productoBuscado == null)
+            {
+
+            }
+            else
+                txtReporte.Text = productoBuscado.ToString();
+        }
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = nuevoAlmacen.reporte();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt32(txtCodigo.Text);
+            string nombre = txtNombre.Text;
+            int cantidad = Convert.ToInt32(txtCantidad.Text);
+            int costo = Convert.ToInt32(txtCosto.Text);
+            nuevoProducto = new Producto(codigo, nombre, cantidad, costo);
+            nuevoAlmacen.insertarProducto(int.Parse(txtInsertar.Text),nuevoProducto);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            nuevoAlmacen.eliminarProducto(Convert.ToInt32(txtCodigo.Text));
+        }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario
+{
+    class Producto
+    {
+        private int _codigo { get; }
+        private string _nombre { get; }
+        private int _cantidad { get; }
+        private int _costo { get; }
+        public Producto siguiente { get; set; }
+        public int codigo
+        {
+            get { return _codigo; }
+        }
+        public string nombre
+        {
+            get { return _nombre; }
+        }
+        public int cantidad
+        {
+            get { return _cantidad; }
+        }
+        public int costo
+        {
+            get { return _costo; }
+        }
+        public Producto(int codigo,string nombre,int cantidad,int costo)
+        {
+            _codigo = codigo;
+            _nombre = nombre;
+            _cantidad = cantidad;
+            _costo = costo;
+        }
+        public override string ToString()
+        {
+            return "Codigo: " + _codigo + "\r\nNombre: "+_nombre + "\r\nCantidad: " +_cantidad +"\r\nCosto: "+_costo+"\r\n";
+        }
+    }
+}
